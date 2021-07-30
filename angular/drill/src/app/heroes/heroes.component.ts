a349ecbb44c55f42845ea4a22b2ccaeb0835f5fb
ToniTSaari <toni.tapani.saari@gmail.com>
2021-07-30 12:23:15 +0300

    Hero.service

--- angular/drill/src/app/heroes/heroes.component.ts
+++ angular/drill/src/app/heroes/heroes.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
-@Component({
+@Component(
+{
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit
+{
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   //hero: Hero = { id:1, name:'Windstorm' }
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
+  getHeroes(): void 
+  {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
   ngOnInit(): void 
   {
-
+    this.getHeroes();
   }
   selectedHero?: Hero;
   onSelect(hero: Hero): void
