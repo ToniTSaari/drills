ea6d209cdfff757705b872d4d832b3b3f92ec603
ToniTSaari <toni.tapani.saari@gmail.com>
2021-08-02 15:45:29 +0300

    Routing intermediate, hero details broken

--- angular/drill/src/app/heroes/heroes.component.ts
+++ angular/drill/src/app/heroes/heroes.component.ts
@@ -15,18 +15,19 @@ export class HeroesComponent implements OnInit
   constructor(private heroService: HeroService, private messageService: MessageService) { }
   //hero: Hero = { id:1, name:'Windstorm' }
   //heroes = HEROES;
-  selectedHero?: Hero;
   heroes: Hero[] = [];
-  
-  onSelect(hero: Hero): void
-  {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
   ngOnInit(): void 
   {
     this.getHeroes();
   }
+  /*
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void
+  {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+  */
   getHeroes(): void 
   {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
